a8432af1884be70465b725fee503a454f1f07f40
Olivier Donzé <olivier.donze@hesge.ch>
2024-10-02 07:04:08 +0200

    first commit

---
+++ impression_3D/multi_mnt_SITG_01_import.py
@@ -0,0 +1,115 @@
+import c4d
+import urllib
+from pathlib import Path
+
+doc: c4d.documents.BaseDocument  # The currently active document.
+op: c4d.BaseObject | None  # The primary selected object in `doc`. Can be `None`.
+
+
+"""Sélectionner toutes les splines converties qui délimitent les zones à découper
+   puis lancer le script
+   Les MNT seront téléchargés dans un dossier MNT_Download à côté du fichier C4D
+   Régler avant les constantes MAX_PX et URL_BASE
+   MAX_PX est la taille maximale de l'image en pixel
+   Ensuite il faut importer les MNT avec avec le script SIG/import MNT geotif dossier complet.py
+   Puis découper les MNT avec le script multi_mnt_SITG_02_decoupe_selon_splines.py
+   
+   ATTENTION si on utilise le WorldElevation il faudra pour l'instant télécharger manuellement chaque url
+   -> Lancer plutôt depuis Visual Studio Code parce qu'on peut cmd/cliquer sur les urls (se connecter avant)"""
+
+CONTAINER_ORIGIN =1026473
+MAX_PX = 1024
+URL_BASE = 'https://raster.sitg.ge.ch/arcgis/rest/services/MNA_TERRAIN_COLLECTION/ImageServer'
+
+#
+URL_BASE = 'https://elevation.arcgis.com/arcgis/rest/services/WorldElevation/Terrain/ImageServer'
+
+
+def emprise_objet(obj, origin):
+    """Calculates the bounding box of the given object.
+
+    Args:
+        obj: The object to calculate the bounding box for.
+        origin: The origin of the bounding box.
+
+    Returns:
+        The bounding box of the object.
+    """
+    mg = obj.GetMg()
+    pts = [p*mg for p in obj.GetAllPoints()]
+
+    xmin = min([p.x for p in pts])+origin.x
+    zmin = min([p.z for p in pts])+origin.z
+    xmax = max([p.x for p in pts])+origin.x
+    zmax = max([p.z for p in pts])+origin.z
+
+    return xmin, zmin, xmax, zmax
+
+def get_url(bbox,resol):
+    xmin, ymin, xmax, ymax = bbox
+    xmin,ymin,xmax,ymax = xmin - resol/2, ymin - resol/2, xmax + resol/2, ymax + resol/2
+    bbox = f'{xmin},{ymin},{xmax},{ymax}'
+
+    size_x = int(round((xmax-xmin)/resol))
+    size_y = int(round((ymax-ymin)/resol))
+
+    url = f'{URL_BASE}/exportImage?'
+    params ={ 'bbox': bbox,
+                'bboxSR': 2056,
+                'size': f'{size_x},{size_y}',
+                'imageSR': 2056,
+                'format': 'tiff',
+                'pixelType': 'F32',
+                'f': 'image'}
+    url += '&'.join([f'{k}={v}' for k,v in params.items()])
+    return url
+
+def main() -> None:
+    """Called by Cinema 4D when the script is being executed.
+    """
+    origin = doc[CONTAINER_ORIGIN]
+    if not origin:
+        c4d.gui.MessageDialog("No origin found.")
+        return
+    # Get the path of the current document.
+    path = Path(doc.GetDocumentPath())
+    if not path:
+        c4d.gui.MessageDialog("Vous devez enregistrer le document avant.")
+        return
+    #create a folder for the TIFF files
+    tiff_folder = path / 'MNT_Download'
+    tiff_folder.mkdir(exist_ok=True)
+
+    name_service = URL_BASE.split('/')[-2]
+
+    for obj in doc.GetActiveObjects(0):
+        bbox = None
+
+        bbox = emprise_objet(obj, origin)
+
+        if not bbox:
+            print(f'No bounding box found for {obj.GetName()}.')
+            continue
+        xmin, zmin, xmax, zmax = bbox
+        width = xmax - xmin
+        height = zmax - zmin
+
+        resol = max(width, height)/(MAX_PX-1) # j'enlève 1 pixel parce qu'on le rajoute après
+        url = get_url(bbox, resol)
+        print(url)
+
+        name_img = f'{name_service}_{obj.GetName()}.tif'
+        fn_tiff = tiff_folder / name_img
+        # verify if the file already exists
+        if fn_tiff.exists():
+            print(f'{fn_tiff} already exists.')
+            continue
+
+        urllib.request.urlretrieve(url, fn_tiff)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ impression_3D/multi_mnt_SITG_02_decoupe_selon_splines.py
@@ -0,0 +1,68 @@
+import c4d
+
+doc: c4d.documents.BaseDocument  # The currently active document.
+op: c4d.BaseObject | None  # The primary selected object in `doc`. Can be `None`.
+
+
+"""Après avoir importer tous les tiff les mettre dans un neutre
+   mettre l'objet contenant les splines juste après
+   l'ordre doit ^être le m^ême pour les deux et dans le nom de chaque MNT
+   on doit retrouver le nom de la spline
+   
+   Attention les MNT seront déplacés un neutre appelés découpe_MNT
+   sera créé avec tous les objets booléens
+   Il faut ensuite manuellement convertir en objet et supprimer
+   ce qui n'est pas MNT"""
+   
+EXTRUSION_VALUE = 5000
+
+def decoupe(mnt,sp):
+    bool_obj = c4d.BaseObject(c4d.Oboole)
+    bool_obj[c4d.BOOLEOBJECT_TYPE] = c4d.BOOLEOBJECT_TYPE_INTERSECT
+    bool_obj[c4d.BOOLEOBJECT_HIGHQUALITY] = False
+    bool_obj.SetMg(sp.GetMg())
+    bool_obj.SetName(mnt.GetName())
+    extr = c4d.BaseObject(c4d.Oextrude)
+    
+    sp_clone = sp.GetClone()
+    sp_clone.InsertUnder(extr)
+    sp_clone.SetRelPos(c4d.Vector(0))
+    extr.InsertUnder(bool_obj)
+    extr[c4d.EXTRUDEOBJECT_DIRECTION] = c4d.EXTRUDEOBJECT_DIRECTION_Y
+    extr[c4d.EXTRUDEOBJECT_EXTRUSIONOFFSET] = EXTRUSION_VALUE
+    
+    mnt_mg = mnt.GetMg()
+    mnt.InsertUnder(bool_obj)
+    mnt.SetMg(mnt_mg)
+    
+    return bool_obj
+
+
+def main() -> None:
+    """Called by Cinema 4D when the script is being executed.
+    """
+    doc.StartUndo()
+    res = c4d.BaseObject(c4d.Onull)
+    res.SetName('decoupes_MNT')
+    doc.InsertObject(res)
+    doc.AddUndo(c4d.UNDOTYPE_NEWOBJ,res)
+    
+    
+    for mnt,sp in zip(op.GetChildren(),op.GetNext().GetChildren()):
+        #on vérifie que le nom de la pièce est bien contenu dans le nom du MNT
+        if sp.GetName() in mnt.GetName():
+            print('ok')
+        else :
+            print(sp.GetName(),mnt.GetName())
+            continue
+        doc.AddUndo(c4d.UNDOTYPE_CHANGE,mnt)
+        boole_obj = decoupe(mnt,sp)
+        boole_obj.InsertUnderLast(res)
+    
+
+    doc.EndUndo()
+    c4d.EventAdd()
+
+
+if __name__ == '__main__':
+    main()
